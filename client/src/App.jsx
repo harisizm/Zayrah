03910a564f006a27ba0e814a423f86e108b90980
harisizm <120340766+harisizm@users.noreply.github.com>
2025-04-28 04:19:47 +0500

    Frontend Continue //
    Product Category pages // click functionality on products// individual product pages// cart icon// cart page// address page // my order page//

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,11 @@ import Footer from './components/Footer';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
+import ProductCategory from './pages/ProductCategory';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import AddAddress from './pages/AddAddress';
+import MyOrders from './pages/MyOrders';
 
 const App = () => {
 
@@ -24,8 +29,13 @@ const App = () => {
 
       <div className={`${isSellerPath ? "" :"px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<AllProducts/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<AllProducts/>} />
+          <Route path='/products/:category' element={<ProductCategory/>} />
+          <Route path='/products/:category/:id' element={<ProductDetails/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/add-address' element={<AddAddress/>}/>
+          <Route path='/my-orders' element={<MyOrders/>}/>
         </Routes>
       </div>
       {!isSellerPath &&<Footer/>}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
  
-  const currency = import.meta.VITE_CURRENCY;
+  const currency = import.meta.env.VITE_CURRENCY;
 
 
   const navigate = useNavigate();
@@ -61,7 +61,28 @@ toast.success("Removed from Cart")
 setCartItems(cartData)
 
 }
+//get cart item count
 
+const getCartCount =()=>{
+  let totalCount = 0;
+  for(const item in cartItems){
+    totalCount +=cartItems[item]
+  }
+  return totalCount;
+}
+
+
+//get cart total amount
+  const getCartAmount =()=>{
+    let totalAmount =0;
+    for(const items in cartItems){
+      let itemInfo = products.find((product)=>product._id === items);
+      if(cartItems[items]>0){
+        totalAmount += itemInfo.offerPrice* cartItems[items]
+      }
+    }
+    return Math.floor(totalAmount* 100)/100;
+  }
 
 
   useEffect(()=>{
@@ -70,7 +91,7 @@ setCartItems(cartData)
 
 
  
-  const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products, currency , addToCart,updateCartItem,removeFromCart,cartItems, searchQuery, setSearchQuery
+  const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products, currency , addToCart,updateCartItem,removeFromCart,cartItems, searchQuery, setSearchQuery,getCartAmount,getCartCount
 
   }
   return (
