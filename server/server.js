97a6bb64a2c8d2efa400f7652a00c3b3b8a433ee
harisizm <120340766+harisizm@users.noreply.github.com>
2025-05-06 04:19:12 +0500

    backened COMPLETED//created all apis for backened
    create api end point //user registration api creation// user login api creation //
    //Seller Features/api (login , logout ,authentication)
    //Api for seller to add item and to display user

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ import cors from 'cors' ;
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/UserRoute.js';
+import sellerRouter from './routes/sellerRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app =express();
 const port = process.env.PORT || 4000;
 
 await connectDB()
+await connectCloudinary()
 
 //Allow multiple origins
 const allowedOrigins= ['http://localhost:5173']
@@ -21,6 +28,12 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res)=> res.send("API is Working") );
 app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
+app.use('/api/order', orderRouter)
+
 
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:${port}`)
