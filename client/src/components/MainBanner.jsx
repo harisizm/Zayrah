dba798a82c146a46cc54e94020e8d821cdfa7882
harisizm <120340766+harisizm@users.noreply.github.com>
2025-05-09 19:16:52 +0500

    redeploy 4

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -17,7 +17,8 @@ const MainBanner = () => {
       />
       <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
         <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-[420px] leading-tight lg:leading-[3.5rem]'>
-          Freshness You Can Trust, Savings You Will Love!
+          Your Everyday Essentials, Elevated - 
+           Discover a smarter way to shop
         </h1>
 
         <div className='flex items-center mt-6 font-medium'>
